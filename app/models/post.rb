add703e8e69a1ec059015b2ff640dc2b1f1292b7
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-10-25 13:09:43 +0700

    Allow to open next and prev post

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,12 @@ class Post
     Pathname(Rails.configuration.story['story_repo'])
   end
 
+  def self.by_file(filepath)
+    path = Pathname(filepath).relative_path_from(self.story_root).
+      to_s.gsub(/\.md$/, '')
+    self.new(path)
+  end
+
   def self.by_url(url)
     story = URLS.find { |i, prefix| url.start_with? prefix }
     raise NotFound unless story
@@ -70,7 +76,37 @@ class Post
     @html
   end
 
+  def prev
+    @prev ||= begin
+      posts = story_posts
+      current = posts.find_index { |i| i == @filepath }
+      return nil if current == 0
+      self.class.by_file(posts[current - 1])
+    end
+  end
+
+  def next
+    @next ||= begin
+      posts = story_posts
+      current = posts.find_index { |i| i == @filepath }
+      return nil if current == posts.length - 1
+      self.class.by_file(posts[current + 1])
+    end
+  end
+
+  def story_name
+    File.dirname(@path).to_sym
+  end
+
+  def url
+    '/' + URLS[self.story_name] + '/' + self.name
+  end
+
   private
+
+  def story_posts
+    @filepath.dirname.children.select { |i| i.to_s.end_with? '.md' }.sort
+  end
 
   def compile
     @compiled = true
