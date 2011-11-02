bbc083ec19cbfdb9cbd307304ec0f2c0624d8030
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-11-02 23:10:33 +0700

    Try to fix unicorn pid path

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -8,7 +8,7 @@ preload_app true
 
 timeout 30
 
-pid "#{shared_dir}/unicorn.pid"
+pid "#{shared_dir}/pids/unicorn.pid"
 
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
