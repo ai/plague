b697b5f733fced4da2500c5d81cf4008b2e62751
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-11-07 19:47:06 +0700

    Add pluralize by R18n helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     check ? { class: cls } : { }
   end
 
+  def pl(count, one, few, many)
+    form = R18n.get.locale.pluralize(count)
+    string = R18n.l(count) + ' '
+    if 1 == form
+      string + one
+    elsif 2 == form
+      string + few
+    else
+      string + many
+    end
+  end
+
 end
