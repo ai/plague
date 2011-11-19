206211b0ae145ea44f38f039b969f0164d94c221
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-11-20 05:18:06 +0800

    Clean up unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -5,24 +5,16 @@ listen "#{shared_dir}/unicorn.sock", backlog: 64
 
 worker_processes 4
 preload_app true
-timeout 30
+timeout 180
 
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
-  old_pid = "#{shared_dir}/pids/unicorn.pid.oldbin"
-  if File.exists?(old_pid) and server.pid != old_pid
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid)
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH; end
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end
