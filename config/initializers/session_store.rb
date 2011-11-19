b24246796b2a024fadd2b37188f720961e03db0d
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-11-19 22:12:01 +0800

    Remember admin session longer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Plague::Application.config.session_store :cookie_store, key: '_plague_session'
+Plague::Application.config.session_store :cookie_store, key: '_plague_session',
+                                                        expire_after: 1.year
