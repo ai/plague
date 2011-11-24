7151800b1dc59befa4670963f1f2af6259255521
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-11-25 01:27:32 +0800

    Try to fix server mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Plague::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Send mail via GMail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:             'smtp.gmail.com',
     port:                '587',
