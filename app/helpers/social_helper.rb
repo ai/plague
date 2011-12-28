a26bb77ba145d7d29d07d2c9cd27a811d5076497
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-12-28 16:47:55 +0700

    Change promo texts

--- app/helpers/social_helper.rb
+++ app/helpers/social_helper.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 module SocialHelper
 
-  def twitter_subscribe(account)
+  def twitter_subscribe
+    account = 'insomnis_story'
     ("<a href=\"https://twitter.com/#{account}\"" +
        ' class="twitter-follow-button"' +
        ' data-button="grey" data-text-color="#FFFFFF" data-link-color="#00AEFF"'+
@@ -9,7 +10,17 @@ module SocialHelper
      "Читать @#{account}</a>").html_safe
   end
 
-  def twitter_share(account, url, text)
+  def twitter_share
+    account = 'insomnis_story'
+    url     =  root_url
+
+    if rand > 0.3
+      text = 'Инсомнис — блог из другого мира. В отличии от книги ' +
+             'вы можете общаться с героем прямо по ходу развития событий.'
+    else
+      text = '2028 год. Эпидемия пришла к людям и они потеряли надежду. ' +
+             'Дьявол соблазняет людей и инквизиция карает невиновных.'
+    end
     ('<a href="https://twitter.com/share"' +
        ' class="twitter-share-button"' +
        " data-url=\"#{url}\" data-text=\"#{text}\" data-related=\"#{account}\""+
